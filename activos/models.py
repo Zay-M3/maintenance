f33a3f63215c9530e5f940b03086b84b8c9c9807
Zay-M3 <oscardavidestradabetancourth@gmail.com>
2025-01-09 20:39:18 -0500

    activos

--- activos/models.py
+++ activos/models.py
@@ -3,18 +3,17 @@ from django.db import models
 # Create your models here.
 
 class activos(models.Model):
-    code_activo = models.BigIntegerField()
-    name_activo = models.CharField(max_length=200)
-    category = models.CharField(max_length=200)
-    num_serie = models.CharField(max_length=10)
-    marc_activo = models.CharField(max_length=50)
-    area_activo = models.CharField(max_length=100)
-    stade_activo = models.CharField(max_length=200)
-    value_activo = models.DecimalField(max_digits=10, decimal_places=4, default=0.0, null = True, blank=True)
-    date_add = models.DateField()
-    life_activo = models.IntegerField(null=True, blank=True)
-    last_maintenance = models.DateTimeField(null=True, blank=True)
-    guarantee_activo = models.CharField(max_length=40, null=True, blank=True)
+    codigoActivo = models.BigIntegerField(unique=True)
+    nombreActivo = models.CharField(max_length=200)
+    categoriaActivo = models.CharField(max_length=200)
+    numeroSerie = models.CharField(max_length=10)
+    marcaActivo = models.CharField(max_length=50)
+    areaActivo = models.CharField(max_length=100)
+    estadoActivo = models.CharField(max_length=200)
+    valorActivo = models.DecimalField(max_digits=10, decimal_places=4, default=0.0, null = True, blank=True)
+    fechaRegistro = models.DateField(auto_now_add=True)
+    ultimoMantenimiento = models.DateTimeField(null=True, blank=True)
+    garantiaActivoTiempo = models.CharField(max_length=200)
     
 
     
--- activos/views.py
+++ activos/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from .models import activos
+from .forms import activosForms
 
 # Create your views here.
 
@@ -14,6 +15,49 @@ class crearActivo(View):
     def get(self, request):
         return render(request, self.templete_name)
     
-    def post(self, request):
-        pass
+    def post(self, request, *args, **kwargs):
+        formActivocreate = activosForms(request.POST)
+
+        if formActivocreate.is_valid():
+            codigoActivo = formActivocreate.cleaned_data['code_activo']
+            nombreActivo = formActivocreate.cleaned_data['name_activo']
+            numeroSerie = formActivocreate.cleaned_data['num_serie']
+            marcaActivo = formActivocreate.cleaned_data['marc_activo']
+            valorActivo = formActivocreate.cleaned_data['value_activo']
+            garantiaActivo = formActivocreate.cleaned_data['guarantee_activo_time']
+            areaActivo = formActivocreate.cleaned_data['area_activo']
+            estadoActivo = formActivocreate.cleaned_data['stade_activo']
+
+            if not isinstance(codigoActivo, int):
+                return render(request, self.templete_name, {
+                'error' : ' ✖️ El codigo debe ser un numero'})
+            if not isinstance(numeroSerie, int):
+                return render(request, self.templete_name, {
+                'error' : ' ✖️ El numero de serie debe ser un numero'})
+            if not isinstance(valorActivo, int):
+                return render(request, self.templete_name, {
+                'error' : ' ✖️ El valor debe ser de tipo numerico'})
+            else: 
+                try:
+                    activo = activos.objects.create(
+                        codigoActivo = codigoActivo,
+                        nombreActivo = nombreActivo,
+                        numeroSerie = numeroSerie,
+                        marcaActivo = marcaActivo,
+                        areaActivo = areaActivo,
+                        estadoActivo = estadoActivo, 
+                        valorActivo = valorActivo,
+                        garantiaActivoTiempo = garantiaActivo
+                    )
+                    activo.save()
+                    return render(request, self.templete_name, {   
+                    'usernew' : ' ✔️ Usuario creado con exito'})
+                except Exception as e:
+                    print(e)    
+                    return render(request, self.templete_name, {
+                    'error' : ' ✖️ Fallo'})
+        else:
+            print(formActivocreate.errors)
+        return render(request, self.templete_name,{'error' : ' ✖️ Fallo al crear usuario'})
+            
     
